373b7246a299148ccd8c04860eb0a096cdfe83c3
lary <laryenockh@gmail.com>
2022-12-01 18:52:55 +0100

    my secong commit

--- test_unit.py
+++ test_unit.py
@@ -6,7 +6,13 @@ from src.cryptographie import chiffreletter
 class TestChiffrement(unittest.TestCase):
     def test_chiffrelettre_function(self):
         a = chiffreletter('B', 'C')
+        b = chiffreletter('C', 'K')
+        c = chiffreletter('A', 'Z')
+        d = chiffreletter('D', 'B')
         self.assertEqual(a, 'D')
+        self.assertEqual(b, 'M')
+        self.assertEqual(c, 'Z')
+        self.assertEqual(d, 'E')
 
 
         
